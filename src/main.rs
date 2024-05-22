87b39067f057a461aed34188308d40041b1680c7
Philipp von Spinn <philipp.vonspinn@e-mundo.de>
2024-05-22 20:27:55 +0200

    added print statements

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@
 use cortex_m_rt::entry;
 use msp432p401r::Peripherals;
 use panic_halt as _;
+use rtt_target::{rprint, rprintln, rtt_init_print};
 
 #[entry]
 fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Start...");
+
+
     let peripherals = Peripherals::take().unwrap();
 
     lib::stop_watchdog_timer(&peripherals);
     lib::set_p1_0_output_dir(&peripherals);
 
     loop {
+        rprintln!("Echo...");
         lib::toggle_p1_0_output(&peripherals);
 
         let mut delay = 100000;
